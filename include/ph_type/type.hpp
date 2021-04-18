a171886adb1ca503e52d3eb5ba046c890a88113b
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-04-18 19:41:22 +0200

    a

--- include/ph_type/type.hpp
+++ include/ph_type/type.hpp
@@ -1,15 +1,16 @@
 #ifndef TYPE_HPP
 #define TYPE_HPP
 #include <ph_concepts/concepts.hpp>
+#include <boost/hof.hpp>
 using namespace std;
 
 template <typename T>
-concept bool_func =
+concept bool_func = requires (T t) {
+    {t ()} -> ph::concepts::convertible <bool>;
+};
 
-template <typename... T>
-requires requires {
+template <bool_func... T>
 
-}
 constexpr auto type_watcher (T&&... t) -> auto
 {
     return [...t = forward <T> (t)] <typename... U> () constexpr -> auto
@@ -35,15 +36,284 @@ constexpr auto type_watcher (T&&... t) -> auto
     };
 }
 
+struct value_type_s {};
+constexpr auto value_type = value_type_s {};
+
+
+
 template <typename T>
 struct type_t
 {
-    //    template <typename Q>
-    //    friend constexpr auto operator== (type_t const&, Q const&) -> bool
-    //    {
-    //        return is_same_v <T, Q>;
-    //    }
+
 };
+template <typename T>
+constexpr type_t <T> type;
+
+//template <auto... t>
+//struct types_t;
+
+template <typename... T>
+struct _types {};
+
+template <typename T>
+struct _types <type_t <T>>
+{};
+
+
+
+//template <auto a, auto, b, auto... u>
+//struct types_t <u..., t>
+//{
+//
+//};
+template <auto const&... t>
+constexpr _types <decay_t <decltype (t)>...> types;
+
+template <typename... T>
+constexpr _types <T...> types_t;
+
+
+
+
+//template <typename... T>
+//requires requires {
+//    requires sizeof... (T) > 0;
+//    requires (is_base_of_v <type_base, T> and ...);
+//}
+//struct type_t : T...
+//{
+//
+//};
+
+template <typename... typifiers>
+struct typify_t
+{
+    
+};
+
+
+
+
+
+
+
+
+template <typename T>
+concept is_type_transf = requires {
+    typename T::transform_types;
+};
+
+
+
+
+
+
+template <typename Derived>
+requires requires {
+    true;
+    /**
+     Derived.transform()
+     
+     because then we can ch
+     */
+    
+}
+struct transformer
+{
+    template <template <typename...> typename T, typename... U>
+    using type = typename Derived::template type <T, U...>::type;
+};
+
+template <typename... T>
+struct transformations;
+
+template <typename Derived, typename... T>
+struct transformations <_types <T...>, transformer <Derived>>
+{
+    
+};
+
+
+
+
+
+
+
+
+
+
+template <ph::concepts::value_type T>
+auto operator | (type_t <T> const&, value_type_s const&) -> auto
+{
+    return typename T::value_type {};
+}
+
+template <typename T, typename U>
+constexpr auto operator!= (type_t <T> const& t, U const& u) -> bool
+{
+    return not (t == u);
+}
+
+template <typename T>
+constexpr auto operator== (type_t <T> const&, type_t <T> const&) -> bool
+{
+    return true;
+}
+
+template <typename T>
+constexpr auto operator!= (type_t <T> const&, type_t <T> const&) -> bool
+{
+    return false;
+}
+
+
+template <typename...>
+struct has_s
+{
+    
+};
+
+
+
+template <typename... T>
+struct common_s
+{
+//    using type = common_type_t <T...>;
+};
+
+template <>
+struct common_s <>
+{
+    template <template <typename...> typename T, typename... U>
+    using type = T <common_type_t <U...>>;
+};
+
+
+
+
+constexpr transformer <common_s <>> common;
+
+
+//template <typename... T, typename... U>
+//constexpr auto typify (typify_t <T...> const&, )
+//{
+//
+//}
+
+
+template <typename U>
+constexpr auto operator > (common_s <> const& n, U&& rhs) -> auto
+{
+    return ;
+}
+
+template <typename T>
+constexpr auto operator < (type_t <T> const&, common_s <> const&) -> auto
+{
+    return common_s <T> {};
+}
+
+//static_assert (is_same_v <decltype (types <int, char> | common), common_s <int, char>>, "");
+
+template <typename... T, typename U>
+constexpr auto operator > (common_s <T...> const& n, U&& rhs) -> auto
+{
+    return common_s <T..., decay_t <U>> {};
+}
+
+
+
+//template <typename... direktiv, typename T>
+//constexpr auto operator < (T&&, common_s <direktiv...> const&) -> auto
+//{
+//    return common_s <decay_t <T>> {};
+//}
+
+
+
+
+
+
+template <typename T, typename Derived1>
+constexpr auto operator | (type_t <type_t <T>> const&, transformer <Derived1> const& t) -> auto
+{
+    return type <T> | t;
+}
+
+
+
+template <typename T, typename Derived1>
+constexpr auto operator | (type_t <T> const&, transformer <Derived1> const& t) -> auto
+{
+    return types_t <T> | t;
+}
+
+template <typename... T, typename Derived1>
+constexpr auto operator | (_types <T...> const&, transformer <Derived1> const& t) -> auto
+{
+    return transformations <_types <T...>, transformer <Derived1>> {};
+}
+
+
+template <typename T, typename Derived1>
+constexpr auto operator | (T&&, transformer <Derived1> const&) -> auto
+{
+    return transformations <transformer <Derived1>, decay_t <T>> {};
+}
+
+static_assert (is_same_v <transformations <_types <int, char, double>, transformer <common_s <>>>, decltype (types_t <int, char, double> | common)>, "");
+
+template <typename T>
+constexpr auto operator or (T&& t, common_s <> const&) -> auto
+{ //cout<<"or"<<endl;
+    return forward <T> (t);
+}
+
+
+//template <typename... T>
+//constexpr auto operator | (bajs_t b, int)
+//{
+//    return b;
+//}
+
+
+template <typename... T, typename... U>
+constexpr auto operator and (type_t <T...> const&, type_t <U...> const&)
+{cout<<"and"<<endl;
+    return type_t <T..., U...> {};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 template <typename T, typename U>
 concept __same = is_same_v <T, U>;
@@ -73,25 +343,5 @@ requires (not has_overloaded_same_type <U, T>)
 {
     return is_same_v <T, U>;
 }
-
-
-template <typename T, typename U>
-constexpr auto operator!= (type_t <T> const& t, U const& u) -> bool
-{
-    return not (t == u);
-}
-
-
-
-
-//template <typename T, typename U>
-//constexpr auto operator== (U const&, type_t <T> const&) -> bool
-//{
-//    return is_same_v <T, U>;
-//}
-
-template <typename T>
-constexpr auto type = type_t <T> {};
-
 
 #endif
--- tests/test_type.cpp
+++ tests/test_type.cpp
@@ -1,18 +1,185 @@
 #include <catch2/catch.hpp>
 #include <ph_type/type.hpp>
 
+using namespace boost::hof;
+
 using namespace std;
+
+
+struct same_f
+{
+    auto operator () (auto a, auto b) const -> bool
+    {
+        return a == b;
+    }
+};
+
+constexpr infix_adaptor <same_f> same = {};
+
+
+
+
+BOOST_HOF_STATIC_LAMBDA_FUNCTION (bajs) = infix ([] (auto x, auto y)
+{
+    return x + y;
+});
+BOOST_HOF_STATIC_LAMBDA_FUNCTION (störst) = infix (BOOST_HOF_LIFT (std::max));
+BOOST_HOF_STATIC_LAMBDA_FUNCTION (minst) = infix (BOOST_HOF_LIFT (std::min));
+
+
+auto inc = [] (auto a)
+{
+    return a + 1;
+};
+
+auto mult = [] (auto a)
+{
+    return a * 2;
+};
+
+
+
+
+
+struct A {};
+struct B : A {
+//    using value_type = int;
+};
+struct C : B {};
+
+struct D {};
+
+struct BASE
+{
+    friend ostream& operator << (ostream& os, BASE const&)
+    {
+        return os;
+    }
+};
+
+
+
+
+
+template <char c>
+struct EL {
+    friend ostream& operator << (ostream& os, EL const& aa)
+    {
+        os << c;
+        return os;
+    }
+};
+
+
+
+template <char... c>
+struct __operator
+{
+    friend ostream& operator << (ostream& os, __operator const& aa)
+    {
+        ((os << c), ...);
+        return os;
+    }
+};
+
+template <template <char...> typename A, template <char...> typename B, char... C, char... D>
+auto operator|(A <C...> a, B <D...> b)
+{
+    return __operator <'(', C..., ' ', '|', ' ', D..., ')'> {};
+}
+template <template <char...> typename A, template <char...> typename B, char... C, char... D>
+auto operator and (A <C...> a, B <D...> b)
+{
+    return __operator <'(', C..., ' ', 'a', 'n', 'd', ' ', D..., ')'> {};
+}
+
+template <template <char...> typename A, template <char...> typename B, char... C, char... D>
+auto operator or (A <C...> a, B <D...> b)
+{
+    return __operator <'(', C..., ' ', 'o', 'r', ' ', D..., ')'> {};
+}
+
+
+//template <template <typename...> typename A, template <typename...> typename B, typename... C, typename... D>
+//auto operator|(A <C...> a, B <D...> b)
+//{
+//    return PIPE <A <C...>, B <D...>> {};
+//}
+//template <template <typename...> typename A, template <typename...> typename B, typename... C, typename... D>
+//auto operator and (A <C...> a, B <D...> b)
+//{
+//    return AND <A <C...>, B <D...>> {};
+//}
+//
+//template <template <typename...> typename A, template <typename...> typename B, typename... C, typename... D>
+//auto operator or (A <C...> a, B <D...> b)
+//{
+//    return OR <A <C...>, B <D...>> {};
+//}
+
+
+TEST_CASE ("")
+{
+//    constexpr auto a = type <int>;
+    auto a = types <type <int>>;
+    
+//    auto dd = common_type_t<A, B> {};
+//    int dsd = 3 <common> 4;
+    
+    transformations <_types <A, B, C>, transformer <common_s <>>> _vv = types_t <A, B, C> | common;
+    
+    auto vv = types_t <A, B, C> | common ;
+    
+//    AA {} and BB {} and CC {} or DD {} and CC {};
+    
+    cout << (EL <'0'> {} and EL <'1'> {} and EL <'2'> {} or EL <'3'> {} and EL <'4'> {} | EL <'5'> {} | EL <'6'> {}) << endl;
+    cout << (EL <'0'> {} or EL <'1'> {} | EL <'x'> {} and EL <'2'> {} or EL <'3'> {} and EL <'4'> {} | EL <'5'> {} | EL <'6'> {}) << endl;
+
+//    AA {} and BB {} | CC {};
+    
+//    AA {} and BB {} | CC {} and DD {};
+    
+//    AA {} and BB {} | CC {} | DD {};
+    
+//    transformations <_types <A, B, C>, transformer <common_s <>>> vv = types_t <A, B, C> | common;
+
+    
+    
+//    common_s <B, C>
+    
+//    auto n = type <B> <common> type <C> | 4;
+    
+    
+//    auto n = type <B> <common> type <C> | value_type;
+    
+//    int n2 = B{} <common> C{} | value_type;
+    
+    constexpr auto sds = compose (inc, mult);
+    
+//    cout << sds (3) << endl;
+    
+    auto d = 4 <störst> 5;
+    auto d2 = 4 <minst> 5;
+//    cout << d2 << endl;
+//    cout << d << endl;
+    auto my_max = BOOST_HOF_LIFT (std::max);
+//    cout << my_max (1, 2) << endl;
+    
+    auto i = 5 <bajs> 4;
+    bool s = 5 <same> 5;
+//    cout << i << endl;
+}
 
 
 
 TEST_CASE ("standard types")
 {
-    int i;
-    REQUIRE (type <int> == i);
-    REQUIRE (i == type <int>);
-    
-    REQUIRE (type <char> != i);
-    REQUIRE (i != type <char>);
+//    int i;
+//    REQUIRE (type <int> == i);
+//    REQUIRE (i == type <int>);
+//
+//    REQUIRE (type <char> != i);
+//    REQUIRE (i != type <char>);
 }
 
 struct inverted
@@ -20,7 +187,7 @@ struct inverted
     template <typename T>
     constexpr auto same_type () const -> bool
     {
-        cout << "overload!" << endl;
+//        cout << "overload!" << endl;
         return false;
     }
 };
@@ -28,7 +195,7 @@ struct inverted
 TEST_CASE ("custom types")
 {
     
-    inverted t;
-    REQUIRE (t != type <inverted>);
-    REQUIRE (not (t == type <inverted>));
+//    inverted t;
+//    REQUIRE (t != type <inverted>);
+//    REQUIRE (not (t == type <inverted>));
 }
